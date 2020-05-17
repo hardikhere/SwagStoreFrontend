d097e28e95247aa865dfedbc801a2650c76c7763
hardikhere <web.hardikhere@gmail.com>
2020-05-17 04:32:22 -0700

    mobile responsive fix

--- src/core/Home.js
+++ src/core/Home.js
@@ -24,13 +24,13 @@ export default function Home() {
   }, []);
 
   return (
-    <Base title="Home Page" description="Welcome to the Tshirt Store">
+    <Base title="Home Page" desc="Welcome to the Tshirt Store">
       <div className="row text-center">
         <h1 className="text-white">All of tshirts</h1>
         <div className="row">
           {products.map((product, index) => {
             return (
-              <div key={index} className="col-4 mb-4">
+              <div key={index} className="col-xs-4 col-md-4 col-sm-4 mb-4>
                 <Card product={product} />
               </div>
             );
